aa182651335f83940358bf22b711b8c457966f1f
anapy <anabelenfunesc@gmail.com>
2020-06-30 18:13:09 +0200

    fase5

--- modulo-02-repaso-funciones/main.js
+++ modulo-02-repaso-funciones/main.js
@@ -8,6 +8,28 @@ function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
 }
 
-writeThis('patata', getRandomNumber(10));
-writeThis('aguacate', getRandomNumber(10));
-writeThis('Pizza', getRandomNumber(10));
+const myWordList = [
+  {
+    text: 'Pencil',
+    total: 6
+  },
+  {
+    text: 'Thermo',
+    total: 2
+  },
+  {
+    text: 'TV',
+    total: 8
+  },
+  {
+    text: 'Phone',
+    total: 4
+  }
+];
+function writeMyArray(myWordList) {
+  for(const word of myWordList) {
+    writeThis(word.text, word.total);
+  }
+}
+
+writeMyArray(myWordList);
