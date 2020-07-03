2f068a34629a69f946b523c19c2cd6821697ae99
anapy <anabelenfunesc@gmail.com>
2020-07-03 20:30:00 +0200

    adding exercise 2 from lesson 10

--- modulo-02/modulo-02- leccion-10-pruebas/main.js
+++
@@ -1,8 +0,0 @@
-function getNumber() {
-  fetch("https://api.rand.fun/integer")
-    .then(response => response.json())
-    .then(data => {
-      document.querySelector('.js-result').innerHTML = data.result
-    });
-}
-document.querySelector('.js-emoji').addEventListener("click", getEmoji);
