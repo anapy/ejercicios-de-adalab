2df53ef2b5dc71f3d13eb6758d0a74f2cd3da4ca
anapy <anabelenfunesc@gmail.com>
2020-07-07 10:42:41 +0200

    adding exercise 3 from lesson 12 incomplete

---
+++ modulo-02/modulo-02-leccion-12-ejercicio-03/main.js
@@ -0,0 +1,55 @@
+'use strict';
+const selectForm = document.querySelector('.select');
+let options = [];
+
+const people = [
+  {
+    name: 'Pepa',
+    lastName: 'Perez',
+    phone: 999999999
+  },
+  {
+    name: 'María',
+    lastName: 'Suarez',
+    phone:123232323,
+  },
+  {
+    name: 'Lucía',
+    lastName: 'Sánchez',
+    phone: 3434324342,
+  }
+];
+function proof() {
+  console.log('hola');
+}
+
+//Crea las etiquetas option con el valor del nombre de cada una
+function createPeople() {
+  for(let i = 0; i < 3; i++) {
+    let option = document.createElement('option');
+    let content = document.createTextNode(people[i].name);
+    option.appendChild(content);
+    option.value = people[i].name;
+    option.classList.add('options');
+    console.log(option);
+    selectForm.appendChild(option);
+    //debugger;
+  }
+}
+
+function magic(event) {
+  options = document.querySelectorAll('.options')
+  console.log(event.currentTarget);
+  proof();
+  for(let i = 0; i < 3; i++) {
+  options[i].addEventListener('change', proof);
+  }
+}
+createPeople();
+selectForm.addEventListener('click', magic);
+
+// Recoge los tres botones cuando se da click a cualquiera de ellos
+// for(let i = 0; i < 3; i++) {
+//   options[i].addEventListener('click', createPeople);
+// }
+
