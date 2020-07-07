061ef3f21cd7d69750f8e8b281584d4bd4816637
anapy <anabelenfunesc@gmail.com>
2020-07-07 18:14:15 +0200

    adding exercise 2 from lesson 13

--- modulo-02/modulo-02-leccion-13-leccion-01/main.js
+++ modulo-02/modulo-02-leccion-13-leccion-01/main.js
@@ -5,3 +5,8 @@ const marks = [5, 4, 6, 7, 9];
 const newMarks = marks.map(mark => mark + 1);
 console.log(newMarks);
 
+//EJERCICIOS 02 -- Saludar es de buena educación //
+const names = ['María', 'Lucía', 'Susana', 'Rocío', 'Inmaculada'];
+const hi = 'Bienvenida ';
+const greetings = names.map(name => 'Bienvenida ' + name); 
+console.log(greetings);
