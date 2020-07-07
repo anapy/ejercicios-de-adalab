f9e014c7c5e21e998a8e5ab4fcba7db62bdf5af2
anapy <anabelenfunesc@gmail.com>
2020-07-07 18:51:01 +0200

    adding exercise 6 from lesson 13

--- modulo-02/modulo-02-leccion-13-leccion-01/main.js
+++ modulo-02/modulo-02-leccion-13-leccion-01/main.js
@@ -21,10 +21,10 @@ const users = [
   { name: 'Rocío', isPremium: false },
   { name: 'Inmaculada', isPremium: false }
 ];
-const premiumGreet = users.map(user => generatePremiumGreet(user));
+const premiumGreet = users.map(user => generateGreet(user));
 console.log(premiumGreet);
 //Revisa si la usuaria es premium o no y devuelve el saludo en concordancia
-function generatePremiumGreet(person) {
+function generateGreet(person) {
   let newGreet;
   if(person.isPremium) {
     newGreet = `Bienvenida ${person.name}. Gracias por confiar en nosotros.`
@@ -34,11 +34,26 @@ function generatePremiumGreet(person) {
   return newGreet;
 }
 
+
 //EJERCICIO 04 -- Solo los premium//
 const premiumUsers = users.filter(user => user.isPremium === true);
 console.log(premiumUsers);
 
+
 //EJERCICIO 05 -- Los pares pueden entrar//
 const pins = [2389, 2384, 2837, 5232, 8998];
-const allowUsers = pins.filter(pin => pin % 2 === 0);
-console.log(allowUsers);
+const allowPins= pins.filter(pin => pin % 2 === 0);
+console.log(allowPins);
+
+
+//EJERCICIO 06 -- Los usuarios que pueden entrar//
+const usersPass = [
+  { name: 'María', isPremium: false, pin: 2389 },
+  { name: 'Lucía', isPremium: true, pin: 2384 },
+  { name: 'Susana', isPremium: true, pin: 2837 },
+  { name: 'Rocío', isPremium: false, pin: 5232 },
+  { name: 'Inmaculada', isPremium: false, pin: 8998 }
+];
+
+const usersAllowed = usersPass.filter(userPass => userPass.pin % 2 === 0);
+console.log(usersAllowed);
