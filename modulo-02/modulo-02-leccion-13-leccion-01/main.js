720d15362d918e0b52e03f50eb702d62b2237df1
anapy <anabelenfunesc@gmail.com>
2020-07-07 18:24:47 +0200

    adding exercise 3 from lesson 13

--- modulo-02/modulo-02-leccion-13-leccion-01/main.js
+++ modulo-02/modulo-02-leccion-13-leccion-01/main.js
@@ -10,3 +10,26 @@ const names = ['María', 'Lucía', 'Susana', 'Rocío', 'Inmaculada'];
 const hi = 'Bienvenida ';
 const greetings = names.map(name => 'Bienvenida ' + name); 
 console.log(greetings);
+
+//EJERCICIOS 03 -- Gracias por confiar en nosotros//
+const users = [
+  { name: 'María', isPremium: false },
+  { name: 'Lucía', isPremium: true },
+  { name: 'Susana', isPremium: true },
+  { name: 'Rocío', isPremium: false },
+  { name: 'Inmaculada', isPremium: false }
+];
+
+const premiumGreet = users.map(user => generatePremiumGreet(user));
+console.log(premiumGreet);
+
+//Revisa si la usuaria es premium o no y devuelve el saludo en concordancia
+function generatePremiumGreet(person) {
+  let newGreet;
+  if(person.isPremium) {
+    newGreet = `Bienvenida ${person.name}. Gracias por confiar en nosotros.`
+  } else {
+    newGreet = 'Bienvenida ' + person.name;
+  }
+  return newGreet;
+}
