b2e190c096f00e5536e59f5c0d632cfcdf6e26e9
anapy <anabelenfunesc@gmail.com>
2020-07-14 18:31:27 +0200

    add lesson3-exercise5: modificate html with conditionals and innerHTML

---
+++ modulo-02/modulo-02-leccion-03-ejercicio-05/main.js
@@ -0,0 +1,16 @@
+const title = document.querySelector('.js-title');
+const text = document.querySelector('.js-text');
+const notification = document.querySelector('.js-notification');
+
+
+if(notification.classList.contains('warning')) {
+  title.innerHTML = 'AVISO';
+  text.innerHTML = 'Tenga cuidado';
+} else if(notification.classList.contains('error')){
+  title.innerHTML = 'ERROR';
+  text.innerHTML = 'Ha surgido un error';
+} else if(notification.classList.contains('success')){
+  title.innerHTML = 'CORRECTO';
+  text.innerHTML = 'Los datos son correctos';
+}
+ 
