030b89a2d090d41254e4810b86cfdcc62cf5aaf0
anapy <anabelenfunesc@gmail.com>
2020-07-15 21:17:59 +0200

    add exercise 3-lesson 1- herency with polygons

---
+++ modulo-03/modulo-03-leccion-01-ejercicio-03/main.js
@@ -0,0 +1,41 @@
+class Polygon {
+  constructor(numberOfSides, base, height) {
+    this.numberOfSides = numberOfSides;
+    this.base = base;    
+    this.height = height;
+  }
+
+  perimeter() {
+    const perimeter = this.numberOfSides * this.base;
+    console.log(`El perímetro es de ${perimeter}`);
+  }
+  area() {
+    const area = this.base * this.height;
+    console.log(`El área es de ${area}`);
+
+  }
+}
+
+//Create class Square with side inside class
+class Square extends Polygon{
+  constructor(side) {
+    super(4, side, side);
+  }
+}
+class Triangle extends Polygon{
+  constructor(base, height) {
+    super(3, base, height);
+  }
+  area() {
+    const area = ((this.base * this.height)/2);
+    console.log(`El área es de ${area}`);
+  }
+}
+
+const square = new Square(4);
+square.perimeter();
+square.area();
+
+const triangle = new Triangle(4, 3);
+triangle.perimeter();
+triangle.area();
