2b5062525dc9e1d9ced4cc5e39371263237a6039
anapy <anabelenfunesc@gmail.com>
2020-07-15 17:16:03 +0200

    add module 3: lesson1 - exercise 2

--- modulo-03/modulo-03-leccion-01-ejercicio-01/main.js
+++ modulo-03/modulo-03-leccion-01-ejercicio-01/main.js
@@ -1,24 +1,6 @@
 'use strict';
-//Create class Square with side inside class
-class Square {
-  sideLength = 9;
-
-  perimeter() {
-    const perimeter = 4 * square.sideLength;
-    console.log(`El perímetro del cuadrado es de ${perimeter}`);
-  }
-  area() {
-    const area = square.sideLength * square.sideLength;
-    console.log(`El área del cuadrado es de ${area}`);
-  }
-}
-
-const square = new Square();
-square.perimeter();
-square.area();
-
 //Create class Square with atribute side outside class
-class Square2 {
+class Square {
   perimeter(side) {
     const perimeter = 4 * side;
     console.log(`El perímetro del cuadrado es de ${perimeter}`);
@@ -29,6 +11,7 @@ class Square2 {
   }
 }
 
-const square2 = new Square2();
-square2.perimeter(9);
-square2.area(9);
+const square = new Square2();
+square.perimeter(9);
+square.area(9);
+
---
+++ modulo-03/modulo-03-leccion-01-ejercicio-02/main.js
@@ -0,0 +1,19 @@
+//Create class Square with side inside class
+class Square {
+  constructor(sideLength) {
+    this.sideLength = sideLength;
+  }
+
+  perimeter() {
+    const perimeter = 4 * this.sideLength;
+    console.log(`El perímetro del cuadrado es de ${perimeter}`);
+  }
+  area() {
+    const area = this.sideLength * this.sideLength;
+    console.log(`El área del cuadrado es de ${area}`);
+  }
+}
+
+const square = new Square(9);
+square.perimeter();
+square.area();
