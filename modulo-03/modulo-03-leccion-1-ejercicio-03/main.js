ed366768624752788b1d7324a51979cd9ed0c0f3
anapy <anabelenfunesc@gmail.com>
2020-07-17 10:09:47 +0200

    add exercise 3-lesson 1- herency with polygons and exp-imp modules

---
+++ modulo-03/modulo-03-leccion-1-ejercicio-03/main.js
@@ -0,0 +1,9 @@
+import polygon from './polygon.js';
+
+const square = new polygon.Square(4);
+square.perimeter();
+square.area();
+
+const triangle = new polygon.Triangle(4, 3);
+triangle.perimeter();
+triangle.area();
