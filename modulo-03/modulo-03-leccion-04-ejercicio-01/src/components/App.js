d3c7644b38bb1c9d5a42cef6d1db489bed7d7662
anapy <anabelenfunesc@gmail.com>
2020-07-21 21:25:23 +0200

    adding exercises 1 - lesson 4 module 3

---
+++ modulo-03/modulo-03-leccion-04-ejercicio-01/src/components/App.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import Item from './Item';
+
+
+class App extends React.Component {
+  render() {
+
+    const arrayOfItems = [
+      {
+        name: 'Cereales con chocolate',
+        description: 'Cereales rellenos de chocolate',
+        quantity: 2,
+        category: 'Cereales',
+        price: 5
+      },
+      {
+        name: 'Hamburguesa con queso',
+        description: 'Hamburguesa rica y saludable',
+        quantity: 1,
+        category: 'Fast-food',
+        price: 15
+      },
+      {
+        name: 'Agua mineral',
+        description: 'Agua de un charco del Himalaya',
+        quantity: 2,
+        category: 'Bebida',
+        price: 5
+      }
+    ];
+    console.log(arrayOfItems[0]);
+  let items = [];
+  for(let i = 0; i < arrayOfItems.length; i++) {
+    items[i] = <li>
+      <Item
+        name= {arrayOfItems[i].name}
+        description={arrayOfItems[i].description}
+        quantity={arrayOfItems[i].quantity}
+        category={arrayOfItems[i].category}
+        price={arrayOfItems[i].price}
+      />
+    </li>
+  }
+  console.log(items);
+    return ( 
+    <ul className="item-list">
+        {items}
+        </ul>
+    );
+  }
+}
+
+export default App;
+
+// //punto b
+// let items = [];
+//   //filtra los precios para solo recoger los que valen menos de 10€
+//   const newarray = arrayOfItems.filter(item => parseInt(item.price) < 10);
+//   for(let i = 0; i < newarray.length; i++) {
+//     items[i] = <li>
+//       <Item
+//         name= {newarray[i].name}
+//         description={newarray[i].description}
+//         quantity={newarray[i].quantity}
+//         category={newarray[i].category}
+//         price={newarray[i].price}
+//       />
+//     </li>
